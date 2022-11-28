4751607c7ffd4dd92ab025c11243e82fb8ced82a
miriamgf <you@example.com>
2022-11-28 19:52:55 +0100

    Upload script

--- let_it_snow.py
+++ let_it_snow.py
@@ -1,29 +1,29 @@
 import turtle
 import numpy as np
-from matplotlib.pyplot import cm
-import random
 
 
 def main(speed=0, bg_color="grey"):
     # create Turtle object
     turtle_screen = turtle.Screen()
     myTurtle = turtle.Turtle()
-
+    
     # set speed to 'fastest = 0'
     myTurtle.speed(speed)
     # change background color
     turtle_screen.bgcolor(bg_color)
+  
+ 
+    """TODO: define different colors here"""
 
-    c = ['steelblue', 'firebrick', 'white', 'darkgreen', 'red', 'purple', 'gold', 'red', 'teal']
 
-    for i in range(10):
+    for _ in range(10):
         # define some params
         size = 18
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
 
         """TODO: set snowflake color here (one of the colors defined above)"""
 
-        myTurtle.color(c[random.randrange(0, 9, 1)])
+        # Go to the start position of the snowflake
         myTurtle.penup()
         myTurtle.goto(pos[0], pos[1])
         myTurtle.pendown()
